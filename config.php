ba1eea00f0ae8971e138fd3c202b95583e6d13f9
EnthalpyEntertainment <endofelnthalpyemail@gmail.com>
2026-05-05 11:48:56 +1000

    Update config.php

--- config.php
+++ config.php
@@ -1,3 +1,7 @@
 <?php
-
+return [
+		"volentersQrRedirect" => "https://docs.google.com/forms/d/e/1FAIpQLSfSLBE9VGfU3EIsayrkXB0F7MdBTrHy4Ugxo1kyEIi3A9isoQ/viewform?usp=header",
+		"latatude" => '34.878167',
+		"logditude" =>'150.603917',
+	];
 ?>
