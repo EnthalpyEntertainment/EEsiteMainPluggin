2cbf4f603883d128f9715caafd78686fdfd9397b
samverheyden <endofelnthalpyemail@gmail.com>
2026-05-05 10:41:44 +1000

    init

---
+++ EEMainSiteMasterPluggin.php
@@ -0,0 +1,22 @@
+<?php
+/*
+Plugin Name: EEMainSiteMasterPluggin
+Description: The master EE pluggin conects and shares data with the satalight sites 
+Version: 1.0.1
+Author: sam verheyden
+*/
+
+
+$apiKey = 'gfdg44353653test';
+
+//auto update
+require_once __DIR__ . '/plugin-update-checker-master/plugin-update-checker.php';
+use YahnisElsts\PluginUpdateChecker\v5\PucFactory;
+$updateChecker = PucFactory::buildUpdateChecker(
+    'https://github.com/samverheyden/google-api-php-client-main/',
+    __FILE__,
+    'google-api-php-client-main'
+);
+$updateChecker->setBranch('main');
+//auto update
+?>
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# EEsiteMainPluggin
+#instalation
+change credentialsTemplate.json to  credentials.json and replace credetials with your new credetials genrated through goolge api
+ change $apiKey = 'youowncustompasscode'; in google api amin php
