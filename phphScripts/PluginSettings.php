d65c87cc86e8d6e8c47524c4c1c3c5b8f60f5c6d
EnthalpyEntertainment <endofelnthalpyemail@gmail.com>
2026-05-07 10:12:03 +1000

    Update PluginSettings.php

--- phphScripts/PluginSettings.php
+++ phphScripts/PluginSettings.php
@@ -47,6 +47,20 @@ echo '    <style>
     
 echo '    <span class="config_rowstyleing_eeplugin"><span class="config_rowstyleing_eeplugin_rowname"> heading</span>longtitude<input id="logditudeelement" type="number"/>latitude <input id="latitudeelement" type="number" /></span>';
 }
-    
+
+add_action('admin_menu', 'eemaster_add_settings_page');
+
+function eemaster_add_settings_page()
+{
+    add_menu_page(
+        'EE Master Settings',     // Page title
+        'EE Master',              // Menu title
+        'manage_options',         // Capability
+        'eemaster-settings',      // Menu slug
+        'Generate_config_settings_page_eemaster', // Callback
+        'dashicons-admin-generic',
+        80
+    );
+}
 
 ?>
